39be4795896e829d6ffb6303a036bb9af33970de
maxgertzen <maxgertzen@gmail.com>
2024-02-03 04:07:18 +0200

    feat(footer): created

---
+++ components/sections/Footer.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ScrollArrow from '../common/ScrollArrow';
+import { Spacer } from '@nextui-org/react';
+
+const Footer: React.FC = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
+  return (
+    <footer className='bg-black text-white p-30 flex flex-col items-center gap-8'>
+      <Spacer y={8} />
+      <ScrollArrow
+        direction='up'
+        className='hover:cursor-pointer border-white'
+        type='arrows'
+        onClick={scrollToTop}
+      />
+      <p>&copy; Built by Max Gertzen</p>
+    </footer>
+  );
+};
+
+export default Footer;
