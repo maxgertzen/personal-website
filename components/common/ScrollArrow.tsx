19ad406e9c990022c359ec4b80f998cdc4aa00cb
maxgertzen <maxgertzen@gmail.com>
2024-02-03 04:06:36 +0200

    fix(style): added arrows view to scroll indicator

--- components/common/ScrollArrow.tsx
+++ components/common/ScrollArrow.tsx
@@ -2,18 +2,42 @@ import React from 'react';
 
 type ScrollArrowProps = {
   direction: 'up' | 'down';
+  type?: 'mouse' | 'arrows';
   className?: string;
+  onClick?: () => void;
 };
 
 const ScrollArrow: React.FC<ScrollArrowProps> = ({
   direction,
+  type = 'mouse',
+  onClick,
   className = '',
 }) => {
+  if (type === 'arrows') {
+    return (
+      <div className={className} onClick={onClick}>
+        <div
+          className={`arrow ${
+            direction === 'down' ? 'arrow-down down' : 'arrow-up up'
+          }-arrow-1`}></div>
+        <div
+          className={`arrow ${
+            direction === 'down' ? 'arrow-down down' : 'arrow-up up'
+          }-arrow-2`}></div>
+        <div
+          className={`arrow ${
+            direction === 'down' ? 'arrow-down down' : 'arrow-up up'
+          }-arrow-3`}></div>
+      </div>
+    );
+  }
+
   return (
     <div
       className={`scroller ${
         direction === 'down' ? 'scroll-down' : 'scroll-up'
-      } ${className}`}></div>
+      } ${className}`}
+      onClick={onClick}></div>
   );
 };
 
