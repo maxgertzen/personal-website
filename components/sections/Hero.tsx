d21351d5671f8c07afd60f83a2e876461ac425d2
maxgertzen <maxgertzen@gmail.com>
2024-02-03 04:07:07 +0200

    feat(mobile): added device context, and setting indicator accordingly

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Section from '../common/Section';
-import { Spacer } from '@nextui-org/react';
 import ScrollArrow from '../common/ScrollArrow';
+import { useDevice } from '../../context/DeviceContext';
 
 const Hero: React.FC = () => {
+  const { isMobile } = useDevice();
+  const scrollToAbout = () => {
+    const about = document.getElementById('about');
+    about?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  };
+
   return (
     <Section id='hero' className='mt-36' fullHeight>
       <h1>
@@ -11,9 +17,14 @@ const Hero: React.FC = () => {
         <br />
         Gertzen
       </h1>
-      <h5 className='tracking-[.02rem]'>Develop. Code. Design. Music.</h5>
+      <h5 className='tracking-[.02rem] ml-2'>Develop. Code. Design. Music.</h5>
       <div className='absolute bottom-80'>
-        <ScrollArrow direction='down' />
+        <ScrollArrow
+          direction='down'
+          className='hover:cursor-pointer'
+          type={isMobile ? 'arrows' : 'mouse'}
+          onClick={scrollToAbout}
+        />
       </div>
     </Section>
   );
