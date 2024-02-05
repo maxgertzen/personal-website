b381fdb214a42d47becacfd5085c1f8041a582e5
maxgertzen <maxgertzen@gmail.com>
2024-02-05 21:14:06 +0200

    fix(contact): aligned required prop

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -6,15 +6,15 @@ type RequestBody = {
   name: string;
   email: string;
   phoneNumber: string;
-  isSubscribed: boolean;
+  isAgreeingToTerms: boolean;
   message: string;
 };
 
 const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { name, email, message, phoneNumber, isSubscribed }: RequestBody =
+  const { name, email, message, phoneNumber, isAgreeingToTerms }: RequestBody =
     req.body;
 
-  if (!name || !email || !message || !isSubscribed) {
+  if (!name || !email || !message || !isAgreeingToTerms) {
     return res.status(400).json({ error: 'Missing required fields' });
   }
 
@@ -31,14 +31,14 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
   const mailOptions = {
     from: email,
     to: process.env.EMAIL_USERNAME,
-    subject: `[maxgertzen.co] - New contact form submission from ${name}`,
+    subject: `[maxgertzen.com] - New contact form submission from ${name}`,
     html: `
       <p>You have a new contact form submission:</p>
       <p><strong>Full Name: </strong> ${name}</p>
       <p><strong>Phone Number: </strong> ${phoneNumber}</p>
       <p><strong>Email: </strong> ${email}</p>
       <p><strong>Agreed to receive communications: </strong> ${
-        isSubscribed ? 'Yes' : 'No'
+        isAgreeingToTerms ? 'Yes' : 'No'
       }</p>
       <p><strong>Message: </strong> ${sanitizedMessage}</p>
     `,
