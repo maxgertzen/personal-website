370d319102bb0208413c491aeadaff95dc6bc2d9
maxgertzen <maxgertzen@gmail.com>
2024-02-06 18:24:18 +0200

    fix(contact): using Script for recaptcha

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,17 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
         <link rel='icon' href='/favicon.png' />
-        <script
+        <Script
           src={`https://www.google.com/recaptcha/api.js?render=${process.env.RECAPTCHA_SITE_KEY}`}
-          async
-          defer></script>
+          onLoad={() => {
+            console.log('reCAPTCHA script loaded!');
+          }}
+        />
       </Head>
       <body>
         <Main />
