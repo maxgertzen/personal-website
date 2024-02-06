d4e60a1c0fd543d12aed5fb01c9d38dc6da4e94b
maxgertzen <maxgertzen@gmail.com>
2024-02-06 19:14:32 +0200

    fix(contact): adding google script in form instead

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,17 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang='en'>
       <Head>
         <link rel='icon' href='/favicon.png' />
-        <Script
-          src={`https://www.google.com/recaptcha/api.js?render=${process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY}`}
-          onLoad={() => {
-            console.log('reCAPTCHA script loaded!');
-          }}
-        />
       </Head>
       <body>
         <Main />
