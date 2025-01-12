788cc14e849a5c185fd697f7ccab8cc891bba4c8
maxgertzen <34470211+maxgertzen@users.noreply.github.com>
2025-01-12 17:20:11 +1100

    feat(contact): integrated with google app (#22)

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,6 +1,5 @@
-import { FormSubmission } from '@/types';
+import { FormSubmission, FormValues } from '@/types';
 import { NextApiRequest, NextApiResponse } from 'next';
-import nodemailer from 'nodemailer';
 import xss from 'xss';
 
 async function verifyRecaptcha(token: string): Promise<boolean> {
@@ -27,6 +26,7 @@ async function verifyRecaptcha(token: string): Promise<boolean> {
       );
 
     const data = await response.json();
+
     console.info(JSON.stringify(data, null, 2));
 
     return data.success;
@@ -35,7 +35,32 @@ async function verifyRecaptcha(token: string): Promise<boolean> {
   }
 }
 
-const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
+const startGoogleApp = async (formValues: FormValues) => {
+  try {
+    const appUrl = process.env.G_APP_URL;
+
+    if (!appUrl) throw new Error('No URL is set');
+
+    const response = await fetch(process.env.G_APP_URL as string, {
+      method: 'POST',
+      body: JSON.stringify(formValues),
+    });
+
+    const data = await response.json();
+
+    console.log(JSON.stringify(data, null, 2));
+
+    if (!response.ok || data.status !== 'success') {
+      throw data || response.status;
+    }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const processEntry = async (req: NextApiRequest, res: NextApiResponse) => {
   const {
     name,
     email,
@@ -57,51 +82,19 @@ const sendEmail = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const sanitizedMessage = xss(message);
 
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: 'smtp.gmail.com',
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  await new Promise((resolve, reject) => {
-    transporter.verify(function (error, success) {
-      if (error) {
-        console.log(error);
-        reject(error);
-      } else {
-        console.log('Server is ready to take our messages');
-        resolve(success);
-      }
-    });
-  });
-
-  const mailOptions = {
-    from: `"maxgertzen.com" <${process.env.EMAIL_USERNAME}>`,
-    to: process.env.EMAIL_USERNAME,
-    subject: `New contact form submission from ${name}`,
-    html: `
-      <p>You have a new contact form submission:</p>
-      <p><strong>Full Name: </strong> ${name}</p>
-      <p><strong>Phone Number: </strong> ${phoneNumber}</p>
-      <p><strong>Email: </strong> ${email}</p>
-      <p><strong>Agreed to receive communications: </strong> ${
-        isAgreeingToTerms ? 'Yes' : 'No'
-      }</p>
-      <p><strong>Message: </strong> ${sanitizedMessage}</p>
-    `,
-  };
-
   try {
-    const emailResponse = await transporter.sendMail(mailOptions);
-    console.log('Message Sent', emailResponse.messageId);
-    res.status(200).json({ message: 'Email sent' });
+    const response = await startGoogleApp({
+      name,
+      email,
+      message: sanitizedMessage,
+      phoneNumber,
+      isAgreeingToTerms,
+    });
+
+    return res.status(200).json(response);
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Error sending email' });
+    return void res.status(500).end();
   }
 };
 
-export default sendEmail;
+export default processEntry;
