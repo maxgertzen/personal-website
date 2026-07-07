8974edb49b45a5862f59a3498fc30b3c781efbc5
maxgertzen <maxgertzen@gmail.com>
2026-07-07 18:34:13 +0700

    perf(components): render static sections as server components
    - drop unnecessary 'use client' from About, Skills, CV, Clients, Contact,
      SocialLinks and SocialIcon; none use hooks, handlers or browser APIs, so they
      were needlessly shipped to and hydrated in the client bundle
    - delete empty, unused components/common/Typography.tsx

--- components/common/SocialIcon.tsx
+++ components/common/SocialIcon.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import type { FC, SVGProps } from 'react';
 
 interface SocialIconProps {
--- components/sections/Skills.tsx
+++ components/sections/Skills.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React, { CSSProperties } from 'react';
 import Section from '../common/Section';
 import SkillItem from '../common/SkillItem';
